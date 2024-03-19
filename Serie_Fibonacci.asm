97f39f9f80135164d77cf5d61b92ba13f2be37b4
Ecocam17 <163794995+Ecocam17@users.noreply.github.com>
2024-03-19 13:40:10 -0500

    Add files via upload

--- Serie_Fibonacci.asm
+++ Serie_Fibonacci.asm
@@ -1,15 +1,21 @@
+# Laboratorio 1: Simulación y optimización de un programa en un procesador escalar segmentado
+# Realizado por: ENRIQUE OLMEDO PADILLA
+# 19-03-2024
+
 .data
+    # Pedir al usuario que digite un numero
     numero_max: .asciiz "Ingrese el número máximo para generar la serie Fibonacci: "
-    result_msg: .asciiz "La serie Fibonacci hasta el número máximo ingresado es: "
-    comma_space: .asciiz ", "
+    mensaje: .asciiz "La serie Fibonacci hasta el número máximo ingresado es: "
+    coma_espacio: .asciiz ", " # Asigna una coma y un espacio entre cada numero
 
 .text
 .globl main
 
+# Programa principal
 main:
     # Pedir al usuario el número máximo para generar la serie Fibonacci
     li $v0, 4           # Cargar el código del servicio de impresión de cadena
-    la $a0, numero_max      # Cargar la dirección de la cadena a imprimir
+    la $a0, numero_max  # Cargar la dirección de la cadena a imprimir
     syscall             # Llamar al servicio de impresión de cadena
 
     li $v0, 5           # Cargar el código del servicio de lectura de entero
@@ -21,7 +27,7 @@ main:
     li $t2, 1           # Inicializar el segundo término de la serie (F(1))
 
     li $v0, 4           # Cargar el código del servicio de impresión de cadena
-    la $a0, result_msg  # Cargar la dirección de la cadena a imprimir
+    la $a0, mensaje     # Cargar la dirección de la cadena a imprimir
     syscall             # Llamar al servicio de impresión de cadena
 
     li $t3, 0           # Inicializar una variable para almacenar el siguiente término de la serie
@@ -34,9 +40,9 @@ main:
     # Si el siguiente término supera el número máximo, salir del bucle
     bge $t1, $t0, exit  
 
-    li $v0, 4           # Cargar el código del servicio de impresión de cadena
-    la $a0, comma_space # Cargar la dirección de la cadena a imprimir (coma y espacio)
-    syscall             # Llamar al servicio de impresión de cadena
+    li $v0, 4            # Cargar el código del servicio de impresión de cadena
+    la $a0, coma_espacio # Cargar la dirección de la cadena a imprimir una coma y un espacio
+    syscall              # Llamar al servicio de impresión de cadena
 
     add $t3, $t1, $t2   # Calcular el siguiente término de la serie (F(n) = F(n-1) + F(n-2))
     move $t1, $t2       # Mover el valor de F(n-1) a F(n-2) para la siguiente iteración
